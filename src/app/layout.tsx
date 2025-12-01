6b488d76ca085bd2006eb6bdd3ae9c9b5bdda65e
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2025-12-01 19:41:48 +0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { PROJECT_CONFIG } from "@/config/project-config";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: PROJECT_CONFIG.name,
+  description: PROJECT_CONFIG.description,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
